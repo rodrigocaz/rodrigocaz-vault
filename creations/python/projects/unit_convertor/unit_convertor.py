8f6f4e1d210d8b6e0042ea62898cb702aa2d23d2
Rodrigo Cazarin <rodrigocazarin08@gmail.com>
2025-05-22 14:12:24 -0600

    m

--- creations/python/projects/unit_convertor/unit_convertor.py
+++ creations/python/projects/unit_convertor/unit_convertor.py
@@ -1,6 +1,7 @@
 from pyfiglet import Figlet
 import csv
 from tabulate import tabulate
+
 def main():
     f = Figlet(font='slant')
     print(f.renderText('UNIT CONVERTOR\n'))
@@ -8,6 +9,15 @@ def main():
     with open ('si.csv', 'r') as file:
         rscv = csv.DictReader(file)
         print(tabulate(rscv, headers = 'keys', tablefmt= 'grid'))
+    print()
+    print('-------FORMAT-------')
+    print('10 m to cm')
+    print('--------------------\n')
+    print('-------OPTIONS--------')
+    print('1. Length')
+    print('2. Time')
+    print('3. Mass')
+    print('4. Temperature')    
 
 if __name__ == '__main__':
     main()
